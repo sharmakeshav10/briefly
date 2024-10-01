818ab36b92c434d13261bdd1991a86df6807e0f8
Keshav Sharma <sharmakeshav0708@gmail.com>
2024-10-01 23:07:56 +0530

    implemented hero section

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { PiArticleNyTimesBold } from "react-icons/pi";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between mx-20 pt-2">
+      <div className="flex items-center">
+        <PiArticleNyTimesBold size={40} />
+        <span className="text-3xl font-semibold">Briefly</span>
+      </div>
+      <button className="bg-slate-500 px-4 rounded-full">Github</button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-center pt-8 text-4xl font-extrabold m-4">
+        Summarize articles with{" "}
+        <span className="text-purple-500">Open AI GPT-4</span>
+      </h1>
+      <p className="text-center m-4">
+        Enhance your productivity with Briefly, our AI article summarizer,
+        designed to provide you with concise overviews of long articles.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
